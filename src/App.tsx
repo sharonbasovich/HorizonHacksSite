0a1bee752a966f6711c6232696e30bcfc8cf1098
unknown <sharon@basovich.com>
2024-10-13 02:40:44 -0400

    first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,94 @@
 import "./App.css";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import animationData from "./assets/Animation - 1728786951301.json";
+import { useRef, useEffect } from "react";
+import Reveal from "./components/Reveal";
 
 function App() {
-  
+  const lottieRef = useRef<LottieRefCurrentProps>(null); // Create a ref for the Lottie animation
+
+  useEffect(() => {
+    if (lottieRef.current) {
+      lottieRef.current.setSpeed(0.5); // Set the speed here
+    }
+  }, []);
+
   return (
     <>
       <div className="screen">
         <h1 className="text typed name">HorizonHacks</h1>
         <h1 className="text year">2024</h1>
         <img src="/logo.svg" className="logo" />
-        <img src="sunset.webp" className="background animated-blur" />
+        <img
+          src="src/assets/sunset.webp"
+          className="background animated-blur"
+        />
+        <div className="scroll">
+          <Lottie lottieRef={lottieRef} animationData={animationData} />
+        </div>
+      </div>
+      <div className="block"></div>
+      <div className="cover even">
+        <div className="column">
+          <Reveal>
+            <div className="content">
+              <h2>What is HorizonHacks?</h2>
+              <ul>
+                <li>
+                  A hackathon run by high school students, for high school
+                  students
+                </li>
+                <li>
+                  We aim to introduce students to the world of programming and
+                  problem-solving and allow experienced students to use their
+                  skills to solve real-world problems
+                </li>
+                <li>
+                  Each year, <b>HorizonHacks</b> offers a new and innovative
+                  theme that challenges the participant's creativity and
+                  technical skills.
+                </li>
+              </ul>
+            </div>
+          </Reveal>
+        </div>
+        <div className="column">
+          <Reveal>
+            <div className="content">
+              <h2>
+                <br />
+                The Theme
+              </h2>
+              <p>
+                For <b className="goofy">HorizonHacks 2024</b>, our theme will
+                be studying. Participants will create a program to assist
+                students in their studying. Key problems surrounding this topic
+                may include procrastination, burnout, and information retention.
+                In the name of fairness, a detailed case will be revealed on the
+                first day so no team has an advantage.
+              </p>
+            </div>
+          </Reveal>
+        </div>
+        <div className="column">
+          <Reveal>
+            <div className="content">
+              <h2>Competition Format</h2>
+              <ul>
+                <li>2 day format</li>
+                <li>
+                  Participants will use the first day to build teams and develop
+                  their ideas
+                </li>
+                <li>
+                  The second day will be a cumulative presentation consisting of
+                  their vision, their process, and demonstration to our panel of
+                  judges.
+                </li>
+              </ul>
+            </div>
+          </Reveal>
+        </div>
       </div>
     </>
   );
---
+++ src/components/Reveal.tsx
@@ -0,0 +1,50 @@
+import { useRef, useEffect } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
+
+interface Props {
+  children: JSX.Element;
+  width?: "fit-content" | "100%";
+}
+
+function Reveal({ children, width = "fit-content" }: Props) {
+  const scrollRef = useRef(null);
+  const isInView = useInView(scrollRef, { once: true });
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView]);
+
+  return (
+    <div ref={scrollRef} style={{ position: "relative", width, overflow: "hidden" }}>
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={mainControls}
+            transition={{ duration: 0.5, delay: 0.25 }}
+            className="content"
+          >
+            {children}
+          </motion.div>
+          <motion.div
+            variants=
+            {{
+              hidden: { left: 0},
+              visible: { left: "100%" },
+            }}
+            initial="hidden" animate={slideControls}
+            transition={{ duration: 0.5, ease: "easeIn" }}
+            style={{position: "absolute", top:4, bottom: 4, left: 0, right: 0, background: "orange", zIndex: 20}}
+            className="content"/>
+        </div>
+  );
+}
+
+export default Reveal;
