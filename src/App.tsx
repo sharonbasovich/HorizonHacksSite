00103c991621c9c606f7b719bb29327be3fbf8ac
unknown <sharon@basovich.com>
2024-10-12 21:41:49 -0400

    first page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  
+  return (
+    <>
+      <div className="screen">
+        <h1 className="text typed name">HorizonHacks</h1>
+        <h1 className="text year">2024</h1>
+        <img src="/logo.svg" className="logo" />
+        <img src="sunset.webp" className="background animated-blur" />
+      </div>
+    </>
+  );
+}
+
+export default App;
