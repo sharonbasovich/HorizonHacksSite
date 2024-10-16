6a57caee8e426cebe575652e18f0e37829cab612
unknown <sharon@basovich.com>
2024-10-16 18:28:29 -0400

    changed animation

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -20,30 +20,42 @@ function Reveal({ children, width = "fit-content" }: Props) {
   }, [isInView]);
 
   return (
-    <div ref={scrollRef} style={{ position: "relative", width, overflow: "hidden" }}>
-          <motion.div
-            variants={{
-              hidden: { opacity: 0, y: 75 },
-              visible: { opacity: 1, y: 0 },
-            }}
-            initial="hidden"
-            animate={mainControls}
-            transition={{ duration: 0.5, delay: 0.25 }}
-            className="content"
-          >
-            {children}
-          </motion.div>
-          <motion.div
-            variants=
-            {{
-              hidden: { left: 0},
-              visible: { left: "100%" },
-            }}
-            initial="hidden" animate={slideControls}
-            transition={{ duration: 0.5, ease: "easeIn" }}
-            style={{position: "absolute", top:4, bottom: 4, left: 0, right: 0, background: "orange", zIndex: 20}}
-            className="content"/>
-        </div>
+    <div
+      ref={scrollRef}
+      style={{ position: "relative", width, overflow: "hidden" }}
+    >
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+        className="content"
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          background: "#969096",
+          zIndex: 20,
+        }}
+        className="content"
+      />
+    </div>
   );
 }
 
